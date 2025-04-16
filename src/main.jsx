f43072d26ca887ac9e700ab4c9ab8d3c0967cc73
0000yuyu <yujin09219@naver.com>
2025-04-16 09:25:00 +0900

    test: env 설정 변경

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 // 개발 환경일 때만 mock 서버 실행
-if (import.meta.env.VITE_MODE === 'development') {
-  const { worker } = await import('./mocks/browser');
-  const { initMockSocket } = await import('./mocks/wsServer');
-
-  await worker.start();
-  initMockSocket();
+if (import.meta.env.MODE === 'development') {
+  (async () => {
+    const { worker } = await import('./mocks/browser');
+    const { initMockSocket } = await import('./mocks/wsServer');
+    await worker.start();
+    initMockSocket();
+  })();
 }
 
 createRoot(document.getElementById('root')).render(
