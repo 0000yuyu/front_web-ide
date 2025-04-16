2628bd91eb9e876fb27b1d312046c7518a1d1851
0000yuyu <yujin09219@naver.com>
2025-04-16 09:27:38 +0900

    fix: mock server 설정 변경

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 // 개발 환경일 때만 mock 서버 실행
-if (import.meta.env.MODE === 'development') {
+if (import.meta.env.VITE_MODE === 'development') {
   (async () => {
     const { worker } = await import('./mocks/browser');
     const { initMockSocket } = await import('./mocks/wsServer');
